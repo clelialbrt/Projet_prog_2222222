35c2fe3b37b3cab8e09a2ed1fec8f224cfc3231a
asselot <80883289+asselot@users.noreply.github.com>
2021-05-13 17:59:59 +0200

    Create TextViewer.cc

---
+++ TextViewer.cc
@@ -0,0 +1,20 @@
+# include <iostream>
+# include "SupportADessin"
+
+
+void TextViewer :: dessine(Systeme const& a_dessiner)
+{
+  
+  
+}
+void TextViewer :: dessine(Montagne const& a_dessiner)
+{
+  
+  
+}
+void TextViewer :: dessine(Ciel const& a_dessiner)
+{
+}
+	
+	
+
