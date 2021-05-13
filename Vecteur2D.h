cf9984fe90a1c14bc305bfef1c22a28708c8af3d
asselot <80883289+asselot@users.noreply.github.com>
2021-05-13 15:30:32 +0200

    Add files via upload

---
+++ Vecteur2D.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <iostream>
+#include <ostream>
+#include <cmath>
+
+class Vecteur2D
+{
+    friend std::ostream& operator<<(std::ostream& sortie, Vecteur2D const& vect);
+    friend class ChampsPotentiels;
+    public:
+    
+    Vecteur2D (int x=0, int y=0) // constructeur par défaut au vecteur nul
+    : coord_x(x), coord_y(y)
+    {}
+    
+    Vecteur2D( Vecteur2D const& autre) // constructeur de copie pour attributs en respect de la "règle d'or" 
+    :coord_x( autre.coord_x), coord_y(autre.coord_y)
+    {}
+    
+    bool operator==(Vecteur2D const& v) const;
+
+    Vecteur2D& operator+= ( Vecteur2D& V); // operateur interne +=
+    
+    Vecteur2D& operator-= ( Vecteur2D& v2); // operateur interne -=    
+    
+    const Vecteur2D operator-();  // opposé 
+    
+    /*const Vecteur2D& operator*=( double x);  // operateur interne *=*/
+    
+    double produit_scal( const Vecteur2D& autre);
+    
+    double norme2();
+    
+    double norme();
+    
+    Vecteur2D& operator~();
+    
+    //Vecteur2D unitaire();
+    
+    protected:
+    double coord_x;
+    double coord_y;
+    
+};
+
+//--------------------------------------------------------------EXTERNE-------------------------------------------------------------//
+
+
+/*Vecteur2D& operator ++ ( Vecteur2D v);  // operateur externe ++
+
+Vecteur2D& operator -- (  Vecteur2D v);  // operateur externe --*/
+		
+Vecteur2D& operator + ( Vecteur2D v1, const Vecteur2D& v2);  // operateur externe +
+		
+Vecteur2D& operator - ( Vecteur2D v1, const Vecteur2D& v2);  // operateur externe -
+		
+/*Vecteur2D& operator * ( double x,const Vecteur2D& v1 );  // operateur externe */
+
+std::ostream& operator<<(std::ostream& sortie, Vecteur2D const& vect);
