9c661b6443b26004264c940dea431de9bde973d2
asselot <80883289+asselot@users.noreply.github.com>
2021-05-13 18:10:22 +0200

    Update TextViewer.cc

--- TextViewer.cc
+++ TextViewer.cc
@@ -5,7 +5,7 @@
 void TextViewer :: dessine(Systeme const& a_dessiner)
 {
   
-  
+  // afficher les différents + faire un constructeur de textviewer + support à dessin
 }
 void TextViewer :: dessine(Montagne const& a_dessiner)
 {
