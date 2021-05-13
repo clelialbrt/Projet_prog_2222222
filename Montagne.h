f930f4bb0403fb8d232ddee236b8ea7edda35ba0
asselot <80883289+asselot@users.noreply.github.com>
2021-05-13 15:27:41 +0200

    Update Montagne.h

--- Montagne.h
+++ Montagne.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream> 
 #include <cmath>
+#include <string>
 
 class Montagne
 {
@@ -9,10 +10,13 @@ class Montagne
 	Montagne(double x, double y, double h, double ox, double oy) : x0(x), y0(y), H(h), ex(ox), ey(oy) {}
 	double altitude(double a, double b) const;
 	
+	void affiche_para();
+	
 	private:
 	double x0;
 	double y0;
 	double H;
 	double ex;
 	double ey;
+	string type;
 };
