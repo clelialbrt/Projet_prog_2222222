ff9752aeb705052fcdf9c8f51a40ecee68c682f7
asselot <80883289+asselot@users.noreply.github.com>
2021-05-13 16:22:10 +0200

    Create Systeme.cc

---
+++ Systeme.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "Systeme.h"
+
+
+
+
+void Systeme :: affiche ()
+{
+	ptr_hill->affiche_para;
+	ptr_champs->affiche();
+	
+}
+virtual void Systeme :: dessine_sur() override
+{ A->dessine(*this); }
+
+virtual void Systeme ::  evolue ()
+{
+	dessine_sur ();
+	
+	
+}
+
+
+virtual void Systeme :: demarre ()
+{
+	ptr_champs ->resolution();
+	&ptr_ciel (&ptr_champs)
+	dessine_sur();
+	evolue();
+}
+
+std::ostream& operator<<(std::ostream& sortie, Systeme const& Sys)
+{
+	Sys.affiche();
+}
