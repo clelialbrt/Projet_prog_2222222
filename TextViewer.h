beb09f4418c47e925940c8ab197ac3cf36f1536b
asselot <80883289+asselot@users.noreply.github.com>
2021-05-13 18:02:29 +0200

    Update TextViewer.h

--- TextViewer.h
+++ TextViewer.h
@@ -2,11 +2,16 @@ pragma once
 # include <iostream>
 # include "SupportADessin"
 
+
 class TextViewer : public SupportADessin
 {
 virtual void dessine(Systeme const&);
 virtual void dessine(Montagne const&);
 virtual void dessine(Ciel const&); 	
 	
+private :
+
+ofstream& flot; 
+
 	
 };
