7554646b335607e603c92ee0dcde5e9d10d22049
asselot <80883289+asselot@users.noreply.github.com>
2021-05-13 15:29:16 +0200

    Create constantes.h

---
+++ constantes.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Physique
+{
+	extern const double g;
+	extern const double vinfini;
+	extern const double Tinfini;
+	extern const double Pinfini;
+	extern const double Pref;
+	extern const double tau;
+	extern const double Meau;
+	extern const double Mair_sec;
+	extern const double R;
+	extern const double hinfini;
+	extern const double cte;
+	//extern const double einfini;
+}
