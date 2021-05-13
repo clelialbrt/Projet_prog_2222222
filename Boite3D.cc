d716d02f4f30de3d8c1e28b28aeb907d59a34f62
asselot <80883289+asselot@users.noreply.github.com>
2021-05-13 16:07:03 +0200

    Create Boite3D.cc

---
+++ Boite3D.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#inlude "Boite3D.h"
+
+using namespace std;
+
+double Boite3D:: getpas()
+{
+	return pas;
+}
+
+int Boite3D:: getNx()
+{
+	return Nx;
+}
+
+int Boite3D:: getNy()
+{ 
+	return Ny;
+} 
+
+int Boite3D:: getNz()
+{
+	return Nz;
+}
+
+int main ()
+  
+ 
+{
+  
+  return 0;
+  
+}
