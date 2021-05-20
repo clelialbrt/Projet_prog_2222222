fcbf1cc45537ada698c617a43db38de6f8260001
asselot <80883289+asselot@users.noreply.github.com>
2021-05-20 08:44:25 +0200

    Update CubeAir.h

--- CubeAir.h
+++ CubeAir.h
@@ -6,14 +6,21 @@ class CubedAir
 {	
   public:
 	CubedAir(bool nuage) : etat(nuage) {}
+	
 	//coordonnees de la vitesse du vent -> méthode ou attribut? voir complément mathématique, est-ce que c'est la méthode vitesse qui nous donne ces valeurs? si oui alors attribut
 	//cest la norme de la vitesse obtenue dans la methode vitesse donc probablement pas... cest plutot au niveau conceptuel
-	double enthalpie(unsigned int pointx, unsigned int pointy, unsigned int pointz, ChampsPotentiels champo);
-	double temperature(unsigned int px, unsigned int py, unsigned int pz, ChampsPotentiels champo1);
-	double pression;
-	double pression_partielle;
-	double taux_dhumidite;
+	
+	void set_enthalpie(unsigned int pointx, unsigned int pointy, unsigned int pointz, ChampsPotentiels champo);
+	void set_temperature(unsigned int px, unsigned int py, unsigned int pz, ChampsPotentiels champo1);
+	double pression(unsigned int px, unsigned int py, unsigned int pz, ChampsPotentiels champo1);
+	double pression_partielle(unsigned int px, unsigned int py, unsigned int pz, ChampsPotentiels champo1);
+	double pression_vapeur_saturante(unsigned int px, unsigned int py, unsigned int pz, ChampsPotentiels champo1);
+	
 	//vu que l'enthalpie dépend de l'altitude est-ce qu'il serait pas mieux de la mettre en méthode du cube d'air comme ça on accède aussi aux coordonnees de la vitesse et tout le monde est content?
+	void set_etat(unsigned int px, unsigned int py, unsigned int pz, ChampsPotentiels champo1);
+	
 	private:
+	double enthalpie;
+	double temperature;
 	bool etat;
 };
