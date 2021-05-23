241044d9587dc2d09d9198a467258aacfd97dd74
asselot <80883289+asselot@users.noreply.github.com>
2021-05-23 20:55:44 +0200

    Update Ciel.h

--- Ciel.h
+++ Ciel.h
@@ -4,12 +4,14 @@
 #include "Boite3D.h"
 #include "CubeAir.h"
 #include "Dessinable.h"
+#include "SupportADessin.h"
 
 class Ciel : public Boite3D Dessinable
 
 {
 	friend class CubedAir;
 	public:
+	virtual void dessine_sur(SupportADessin a_dessiner)
 	Ciel(const ChampsPotentiels& champ) : Boite3D(champ.Nx, champ.Ny, champ.Nz, champ.pas) {}
 	
 	
