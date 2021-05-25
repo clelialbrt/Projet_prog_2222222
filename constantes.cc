a57e682ba40225fd1ffcf59d470847b7f3a0a0de
asselot <80883289+asselot@users.noreply.github.com>
2021-05-25 21:19:02 +0200

    Update constantes.cc

--- constantes.cc
+++ constantes.cc
@@ -2,12 +2,12 @@
 
 //Fichier contenant toutes les constantes physiques qui sont nécessaires dans certaines classes
 
-	extern const double Physique::g(9.81);                                // accélération gravitationnelle
-
+	extern const double Physique::g(9.81);                                // Accélération gravitationnelle
+        extern const double Physique::vinfini(15.)                            // Vitesse aux bords
 	extern const double Physique::Tinfini(284.5);                         // Température aux bords
 	extern const double Physique::Pinfini(101325);                        // Pression de l'air au bord
 	extern const double Physique::Pref(101325);                           // Pression de référence
-	extern const double Physique::tau(8.10e-3);                           // taux d'humidité
+	extern const double Physique::tau(8.10e-3);                           // Taux d'humidité
 	extern const double Physique::Meau(0.0180153);                        // Masse molaire eau
 	extern const double Physique::Mair_sec(0.02896);                      // Masse molaire air sec
 	extern const double Physique::R(8.3144621);                           // Constante des gaz parfait
