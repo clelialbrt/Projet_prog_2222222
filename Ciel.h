d3e081d249d55218ad0a60369de5c8cb55a7dd68
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-05-25 20:57:16 +0200

    Update Ciel.h

--- Ciel.h
+++ Ciel.h
@@ -1,19 +1,46 @@
-#prama once 
+#pragma once 
 #include <iostream>
 #include <vector>
 #include "Boite3D.h"
-#include "CubeAir.h"
+#include "CubedAir.h"
 #include "Dessinable.h"
 #include "SupportADessin.h"
+#include "ChampsPotentiels.h"
 
-class Ciel : public Boite3D Dessinable
-
+class Ciel : public Boite3D, public Dessinable
 {
 	friend class CubedAir;
 	public:
-	virtual void dessine_sur(SupportADessin a_dessiner)
-	Ciel(const ChampsPotentiels& champ) : Boite3D(champ.Nx, champ.Ny, champ.Nz, champ.pas) {}
+	Ciel(const ChampsPotentiels& champ) : Boite3D(champ.Nx, champ.Ny, champ.Nz, champ.pas) 
+	{
+		for (unsigned int i(0); i < champ.Nx; ++i)
+		{
+			for (unsigned int j(0); j < champ.Ny; ++j)
+			{
+				for (unsigned int k(0); k < champ.Nz; ++k)
+				{
+					cubes_dair[i][j][k].set_vitesse(champ.vitesse(i, j, k)[0], champ.vitesse(i, j, k)[1], champ.vitesse(i, j, k)[2]);
+				}
+			}
+		}
+	}
 	
+	Ciel(double Lx, double Ly, double Lz, double lambda) : Boite3D(Lx, Ly, Lz, lambda) 
+	{
+		for (unsigned int i(0); i < Lx/lambda; ++i)
+		{
+			for (unsigned int j(0); j < Ly/lambda; ++j)
+			{
+				for (unsigned int k(0); k < Lz/lambda; ++k)
+				{
+					cubes_dair[i][j][k].set_vitesse(Physique::vinfini, 0, 0);
+				}
+			}
+		}
+	}
+	
+	CubedAir precedente(double pas_temps, double x, double y, double z);
+	virtual void dessine_sur(SupportADessin a_dessiner);
 	
 	private:
 	std::vector<std::vector<std::vector<CubedAir>>> cubes_dair;
