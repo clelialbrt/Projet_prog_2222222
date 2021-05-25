22c6211e5c1a8757deac969c982b119d40461b75
asselot <80883289+asselot@users.noreply.github.com>
2021-05-25 21:16:41 +0200

    Update constantes.h

--- constantes.h
+++ constantes.h
@@ -2,16 +2,15 @@
 
 namespace Physique
 {
-	extern const double g;           // accélération gravitationnelle
+	extern const double g;           // Accélération gravitationnelle
 	extern const double vinfini;     // Vitesse du vent aux bords (utilisée à l'initialisation)
 	extern const double Tinfini;     // Température aux bords
 	extern const double Pinfini;     // Pression de l'air au bord
 	extern const double Pref;        // Pression de référence
-	extern const double tau;         // taux d'humidité
+	extern const double tau;         // Taux d'humidité
 	extern const double Meau;        // Masse molaire eau
 	extern const double Mair_sec;    // Masse molaire air sec
 	extern const double R;           // Constante des gaz parfait
 	extern const double hinfini;     // Enthalpie aux bords
 	extern const double cte;         // Constante de Bernouilli
-	//extern const double einfini;
 }
