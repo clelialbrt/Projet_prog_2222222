32c21270c96e49e37ea33e2fc01fdd2a5649f2a3
asselot <80883289+asselot@users.noreply.github.com>
2021-05-25 21:09:42 +0200

    Update constantes.cc

--- constantes.cc
+++ constantes.cc
@@ -1,14 +1,14 @@
 #include "constantes.h"
 
+//Fichier contenant toutes les constantes physiques qui sont nécessaires dans certaines classes
 
-	extern const double Physique::g(9.81);
-	extern const double Physique::vinfini(15);
-	extern const double Physique::Tinfini(284.5);
-	extern const double Physique::Pinfini(101325);
-	extern const double Physique::Pref(101325);
-	extern const double Physique::tau(8.10e-3);
-	extern const double Physique::Meau(0.0180153);
-	extern const double Physique::Mair_sec(0.02896);
-	extern const double Physique::R(8.3144621);
-	extern const double Physique::hinfini((7/2)*(R/Mair_sec)*Tinfini);
-	extern const double Physique::cte(0.5*vinfini*vinfini + hinfini);
+	extern const double Physique::g(9.81);                                // accélération gravitationnelle
+	extern const double Physique::Tinfini(284.5);                         // Vitesse du vent au bord (utilisée à l'initialisation)
+	extern const double Physique::Pinfini(101325);                        // Pression de l'air au bord
+	extern const double Physique::Pref(101325);                           // Pression de référence
+	extern const double Physique::tau(8.10e-3);                           // taux d'humidité
+	extern const double Physique::Meau(0.0180153);                        // Masse molaire eau
+	extern const double Physique::Mair_sec(0.02896);                      // Masse molaire air sec
+	extern const double Physique::R(8.3144621);                           // Constante des gaz parfait
+	extern const double Physique::hinfini((7/2)*(R/Mair_sec)*Tinfini);    // Enthalpie aux bords
+	extern const double Physique::cte(0.5*vinfini*vinfini + hinfini);     // Constante de Bernouilli
