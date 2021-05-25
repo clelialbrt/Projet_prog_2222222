6a1ac7a989597cd7a071f8427a09557b177a77a2
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-05-25 21:12:37 +0200

    Update Systeme.h

--- Systeme.h
+++ Systeme.h
@@ -2,36 +2,36 @@
 #include <iostream>
 #include "Montagne.h"
 #include "Ciel.h"
-#include "ChampsPotentiels"
+#include "ChampsPotentiels.h"
 
 
 class Systeme : public Dessinable // rep ce qui forme un système physique
 {
-public :
+	public :
 
-Systeme ( Montagne& M), Ciel& C, ChampsPotentiels& Ch)
-: ptr_champs(*Ch), ptr_ciel(*C), ptr_hill(*M) {}
+	Systeme ( Montagne& M), Ciel& C, ChampsPotentiels& Ch)
+	: ptr_champs(*Ch), ptr_ciel(*C), ptr_hill(*M) {}
 
-~Systeme() 
-{ 
-	delete ptr_champs;
-	delete ptr_ciel;
-	delete ptr_hill;
-}
+	~Systeme() 
+	{ 
+		delete ptr_champs;
+		delete ptr_ciel;
+		delete ptr_hill;
+	}
 
-virtual void affiche ();
-virtual void demarre ();
-virtual void dessine_sur() override;
+	virtual void affiche ();
+	virtual void demarre ();
+	virtual void dessine_sur() ; //override;
 
 
-private : 
+	private : 
 
-ChampsPotentiels* ptr_champs;
-Ciel* ptr_ciel;
-Montagne* ptr_hill;
-virtual void evolue ();
+	ChampsPotentiels* ptr_champs;
+	Ciel* ptr_ciel;
+	Montagne* ptr_hill;
+	virtual void evolue ();
 
-friend std::ostream& operator<<(std::ostream& sortie, Systeme const& Sys);
+	friend std::ostream& operator<<(std::ostream& sortie, Systeme const& Sys);
 
 /*vector <Montagne*> ptr_hill;*/
 	
