78c98a966b2029329107c28b6364260ce59a7b84
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-05-25 20:52:20 +0200

    Update Boite3D.h

--- Boite3D.h
+++ Boite3D.h
@@ -6,10 +6,10 @@ class Boite3D
 {
 	public:
 	Boite3D(unsigned int nomx, unsigned int nomy, unsigned int nomz, double ps) : Nx(nomx), Ny(nomy), Nz(nomz), pas(ps) {}
-	int getNx();
-	int getNy();
-	int getNz();
-	double getpas();
+	int getNx() const;
+	int getNy() const;
+	int getNz() const;
+	double getpas() const;
 	
 	protected:
 	unsigned int Nx;
