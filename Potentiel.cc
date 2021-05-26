8e329431ad673afa5a4b676e50b48002bad2b044
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-05-26 23:03:59 +0200

    Add files via upload

---
+++ Potentiel.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include "Potentiel.h"
+
+void Potentiel:: affiche()
+{
+	cout << poten << endl; // on aurait echange les calculs du potentiel et du laplacien? ah non cest vrai qu'ils sont tous les deux faux :)
+}
